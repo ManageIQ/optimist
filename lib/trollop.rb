7705ec1dfd7f0366ec635eca674aafc965fa2da9
Keenan Brock <keenan@thebrocks.net>
2015-03-10 21:29:21 -0400

    History 2.1.2

--- lib/trollop.rb
+++ lib/trollop.rb
@@ -6,7 +6,7 @@
 require 'date'
 
 module Trollop
-VERSION = "2.1.1"
+VERSION = "2.1.2"
 
 ## Thrown by Parser in the event of a commandline error. Not needed if
 ## you're using the Trollop::options entry.
